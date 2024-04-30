f89355a0ff3122a9b64930d70021100625fe2cfc
Yasminemaroukk <121737352+Yasminemaroukk@users.noreply.github.com>
2024-04-30 14:20:16 +0100

    Create extract_vars.py

---
+++ extract_vars.py
@@ -0,0 +1,17 @@
+import os
+
+def extract_secret_vars():
+    branch = os.getenv("GITHUB_REF")
+    if branch:
+        prefix = "DEV_" if branch == "refs/heads/dev" else "PROD_"
+        path_var = os.getenv(prefix + "PATH")
+        if path_var:
+            with open(".env", "w") as env_file:
+                env_file.write(f"PATH={path_var}")
+        else:
+            raise ValueError(f"{prefix}PATH not found in secrets.")
+    else:
+        raise ValueError("GITHUB_REF environment variable not set.")
+
+if __name__ == "__main__":
+    extract_secret_vars()
