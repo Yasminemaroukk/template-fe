24627e7ce8b3e300b1e7f4fc1b96d4d0ffafea5d
Yasminemaroukk <121737352+Yasminemaroukk@users.noreply.github.com>
2024-05-02 10:11:11 +0100

    Update extract_vars.py

--- extract_vars.py
+++ extract_vars.py
@@ -1,7 +1,9 @@
 import os
 
 def extract_secret_vars():
+    print(os.environ)  # Print all environment variables for debugging
     branch = os.getenv("GITHUB_REF")
+    print("Branch:", branch)  # Print the branch for debugging
     if branch:
         if branch == "refs/heads/dev":
             prefix = "DEV_"
@@ -11,6 +13,7 @@ def extract_secret_vars():
             raise ValueError("Unsupported branch for deployment.")
         
         path_var = os.getenv(prefix + "PATH")
+        print(prefix + "PATH:", path_var)  # Print the secret variable for debugging
         if path_var:
             with open(".env", "w") as env_file:
                 env_file.write(f"{prefix[:-1]}={path_var}")  # Write variable name without prefix to .env
