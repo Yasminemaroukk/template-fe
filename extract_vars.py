8ea4169c2db631c3b1dd1d92401f5ee7c0cb9a3e
Yasminemaroukk <121737352+Yasminemaroukk@users.noreply.github.com>
2024-05-02 08:24:33 +0100

    Update extract_vars.py

--- extract_vars.py
+++ extract_vars.py
@@ -2,9 +2,12 @@ import os
 
 def extract_secret_vars():
     branch = os.getenv("GITHUB_REF")
+    print("Branch:", branch)  # Add this line for debug
     if branch:
         prefix = "DEV_" if branch == "refs/heads/dev" else "PROD_"
+        print("Prefix:", prefix)  # Add this line for debug
         path_var = os.getenv(prefix + "PATH")
+        print("Path Variable:", path_var)  # Add this line for debug
         if path_var:
             with open(".env", "w") as env_file:
                 env_file.write(f"PATH={path_secret}")
@@ -15,4 +18,3 @@ def extract_secret_vars():
 
 if __name__ == "__main__":
     extract_secret_vars()
-
